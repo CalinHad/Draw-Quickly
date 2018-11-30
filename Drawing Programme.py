c71e47fec61a74e474a9d4f92057f942bd2f9048
CalinHad <33909991+CalinHad@users.noreply.github.com>
2018-11-30 10:28:21 +0000

    User Interfaces features (drawing prompt, timer ,colour pallete).

--- Drawing Programme.py
+++ Drawing Programme.py
@@ -1,12 +1,30 @@
 import pygame
 from pygame.locals import*
+black=(0,0,0)
 white=(255,255,255)
+grey=(115,115,115)
+orange=(230,115,0)
 class Canvas():
     def __init__(self, screen_size):
         self.screen_size=screen_size
         self.screen=pygame.Rect(0,0,*self.screen_size)
         pygame.init()
         self.game_display=pygame.display.set_mode(screen_size)
+    def drawing_prompt_box(self):
+        self.drawing_prompt_border=pygame.Rect(0,0,200,100)
+        self.drawing_prompt=pygame.Rect(1,1,198,98)
+        pygame.draw.rect(self.game_display,black,self.drawing_prompt_border)
+        pygame.draw.rect(self.game_display,grey,self.drawing_prompt)
+    def timer_prompt_box(self):
+        self.timer_box_border=pygame.Rect(200,0,200,100)
+        self.timer_box=pygame.Rect(200,1,198,98)
+        pygame.draw.rect(self.game_display,black,self.timer_box_border)
+        pygame.draw.rect(self.game_display,grey,self.timer_box)
+    def colour_pallete(self):
+        self.colours_box_border = pygame.Rect(400, 0, 800, 100)
+        self.colours_box = pygame.Rect(399, 1, 798, 98)
+        pygame.draw.rect(self.game_display, black, self.colours_box_border)
+        pygame.draw.rect(self.game_display, orange, self.colours_box)
     def event_handler(self):
         for event in pygame.event.get():
             if event.type==QUIT:
@@ -15,7 +33,11 @@ class Canvas():
     def run(self):
         clock=pygame.time.Clock()
         while True:
+
             self.game_display.fill(white)
+            self.drawing_prompt_box()
+            self.timer_prompt_box()
+            self.colour_pallete()
             self.event_handler()
             pygame.display.update()
             clock.tick(15)
@@ -23,3 +45,9 @@ if __name__=="__main__":
     size =(1200, 800)
     new_game = Canvas(size)
     new_game.run()
+
+class Brush():
+    def __init__(self):
+        self.mouse_position_x=0
+        self.mouse_position_y=0
+
